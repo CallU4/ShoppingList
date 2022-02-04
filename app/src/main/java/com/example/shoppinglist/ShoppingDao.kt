ca8b22ffd2f37723480c3008aa98a5466190789d
Alex <hqwa.sm1@mail.ru>
2022-02-04 11:10:52 +0300

    Initial commit

---
+++ app/src/main/java/com/example/shoppinglist/ShoppingDao.kt
@@ -0,0 +1,13 @@
+package com.example.shoppinglist
+
+import androidx.room.*
+
+@Dao
+interface ShoppingDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(item: ShoppingItem)
+
+    @Delete
+    suspend fun delete(item: ShoppingItem)
+}
---
+++ app/src/main/java/com/example/shoppinglist/ShoppingItem.kt
@@ -0,0 +1,12 @@
+package com.example.shoppinglist
+
+import androidx.room.*
+
+@Entity(tableName = "shopping_items")
+data class ShoppingItem(
+    @ColumnInfo(name = "item_name") var name: String,
+    @ColumnInfo(name = "item_amount") var amount: Int
+) {
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? = null
+}
